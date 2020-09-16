1e8b3d88a51aacc80131824f142eab6b871768a1
Harshavardhan, Abhishek | RIEPL <abhish.harshavardhan@rakuten.com>
2020-09-16 19:22:25 +0530

    duplicate letters completed

---
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -0,0 +1,17 @@
+function getMax() {
+  return (prev, next) => (prev > next ? prev : next);
+}
+
+function duplicateLetters(...args) {
+  const charMap = {};
+  for (const char of String(args).split('')) {
+    if (charMap[char]) charMap[char] += 1;
+    else charMap[char] = 1;
+  }
+
+  const max = Object.values(charMap).reduce(getMax(), 0);
+
+  return max > 1 ? max : false;
+}
+
+export { duplicateLetters };
